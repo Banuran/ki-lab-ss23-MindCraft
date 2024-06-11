dbe219515d578ec7734a9c7b3309c5688f9d80bb
Ruwen Christ <chru1011@h-ka.de>
2024-06-11 23:21:19 +0200

    first try to integrate hugging face trainer

--- dataset.py
+++ dataset.py
@@ -3,15 +3,18 @@ from PIL import Image
 from torchvision import transforms as tt
 import pandas as pd
 import numpy as np
+import torch
 
 IMAGE_SIZE = 256
 BATCH_SIZE = 1
 
 
 class VisualWSDDataset(Dataset):
-    def __init__(self, mode="train", image_transform=None, text_transform=None, test_lang='en'):
+    def __init__(self, mode="train", image_transform=None, text_transform=None, tokenizer=None, test_lang='en'):
         self.image_transform = image_transform
         self.text_transform = text_transform
+        self.tokenizer = tokenizer
+        self.mode = mode
 
         self.base_path = './data/semeval-2023-task-1-V-WSD-train-v1/train_v1/'
         self.data_txt_path = self.base_path + 'train.data.v1.txt'
@@ -38,6 +41,8 @@ class VisualWSDDataset(Dataset):
         self.data_df = pd.read_csv(self.data_txt_path, delimiter = "\t", header=None)
         self.gold_df = pd.read_csv(self.gold_txt_path, delimiter = "\t", header=None)
         self.data_gold_df  = pd.concat([self.data_df.iloc[:, 0], self.data_df.iloc[:, 1], self.gold_df.iloc[:, 0]], axis=1, keys=['label', 'label_context', 'img_name'])
+        if self.tokenizer != None:
+            self.gold_token = self.tokenizer(self.data_gold_df['label_context'].to_list())
 
     def __len__(self):
         return len(self.data_gold_df)
@@ -49,9 +54,13 @@ class VisualWSDDataset(Dataset):
         images_series = self.data_df.iloc[idx][2:]
         correct_image_idx = images_series[images_series == correct_image_name].index[0]-2
         images = []
-        for item in images_series:
-                images.append(Image.open(self.image_path + item).convert('RGB'))
-        correct_image = images[correct_image_idx]
+
+        if self.mode == "test":
+            for item in images_series:
+                    images.append(Image.open(self.image_path + item).convert('RGB'))
+            correct_image = images[correct_image_idx]
+        else:
+            correct_image = Image.open(self.image_path + images_series[correct_image_idx+2]).convert('RGB')
 
         if self.image_transform:
             correct_image = self.image_transform(correct_image)
@@ -62,5 +71,11 @@ class VisualWSDDataset(Dataset):
             label = self.text_transform(label)
             label_context = self.text_transform(label_context)
 
+        if self.tokenizer != None:
+            # labels are the correct images
+            # input_ids and attention_mask are tokenized text
+            item = {key: torch.tensor(val[idx]) for key, val in self.gold_token.items()}
+            item['images'] = torch.tensor(correct_image)
+            return item
 
         return {'label': label, 'label_context': label_context, 'correct_idx': correct_image_idx, 'correct_img': correct_image, 'imgs': images}
