c5f9f179f9f16e89e3f9a3014ab3a6ed76da027a
2m0a0x0 <selmaniesner@outlook.de>
2024-06-20 17:07:39 +0200

    ALIGN

---
+++ align_model.py
@@ -0,0 +1,134 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torchvision import models
+from transformers import AutoModel, AutoTokenizer, BertTokenizer
+
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"#
+#import train
+from torchvision.models import efficientnet_b0
+from transformers import ViTModel, ViTImageProcessor
+from transformers import GPT2Model, GPT2Tokenizer
+import timm
+
+BATCH_SIZE = 128
+EMBED_DIM = 1024
+TRANSFORMER_EMBED = 768
+IMAGE_SIZE = 224
+
+class Projection(nn.Module):
+    def __init__(self, d_in: int, d_out: int, p: float=0.5) -> None:
+        super().__init__()
+        self.linear1 = nn.Linear(d_in, d_out, bias=False)
+        self.linear2 = nn.Linear(d_out, d_out, bias=False)
+        self.layer_norm = nn.LayerNorm(d_out)
+        self.drop = nn.Dropout(p)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        embed1 = self.linear1(x)
+        embed2 = self.drop(self.linear2(F.gelu(embed1)))
+        embeds = self.layer_norm(embed1 + embed2)
+        return embeds
+
+class VisionEncoder(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.base = timm.create_model('convnext_base', pretrained=True)
+        self.base.head.fc = nn.Identity()
+
+        for p in self.base.parameters():
+            p.requires_grad = False
+
+    def forward(self, x):
+        return self.base(x)
+
+class TextEncoder(nn.Module):
+    def __init__(self, d_out: int) -> None:
+        super().__init__()
+        self.base = AutoModel.from_pretrained("distilbert-base-multilingual-cased")
+        self.projection = Projection(TRANSFORMER_EMBED, d_out)
+        #self.projection = ExtendedProjection(TRANSFORMER_EMBED, d_out)
+        for p in self.base.parameters():
+            p.requires_grad = False
+
+    def forward(self, input_ids, attention_mask):
+        out = self.base(input_ids, attention_mask=attention_mask)[0]
+        out = out[:, 0, :]  # get CLS token output
+        projected_vec = self.projection(out)
+        projection_len = torch.norm(projected_vec, dim=-1, keepdim=True)
+        return projected_vec / projection_len
+    
+class Tokenizer:
+    def __init__(self, tokenizer: BertTokenizer) -> None:
+        self.tokenizer = tokenizer
+
+    def __call__(self, x: str) -> AutoTokenizer:
+        return self.tokenizer(
+            x, truncation=True, padding=True, return_tensors="pt"
+        )
+    
+class CustomModel(nn.Module):
+    def __init__(self, lr: float = 1e-3) -> None:
+        super().__init__()
+        self.vision_encoder = VisionEncoder()
+        self.caption_encoder = TextEncoder(EMBED_DIM)
+        self.tokenizer = Tokenizer(AutoTokenizer.from_pretrained("distilbert-base-multilingual-cased"))
+        self.lr = lr
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.name = "CustomModel"
+
+    def forward(self, images, text):
+        text = self.tokenizer(text).to(self.device)
+
+        image_embed = self.vision_encoder(images)
+        caption_embed = self.caption_encoder(text["input_ids"], text["attention_mask"])
+
+        image_embed = F.normalize(image_embed, p=2, dim=-1)
+        caption_embed = F.normalize(caption_embed, p=2, dim=-1)
+
+        similarity = caption_embed @ image_embed.T
+
+        loss = self.CLIP_loss(similarity)
+        img_acc, cap_acc = metrics(similarity)
+
+        return loss, img_acc, cap_acc
+    
+    def CLIP_loss(self, logits: torch.Tensor) -> torch.Tensor:
+        n = logits.shape[1]      # number of samples
+        labels = torch.arange(n).to(self.device) # Create labels tensor
+        # Calculate cross entropy losses along axis 0 and 1
+        loss_i = F.cross_entropy(logits.transpose(0, 1), labels, reduction="mean")
+        loss_t = F.cross_entropy(logits, labels, reduction="mean")
+        # Calculate the final loss
+        loss = (loss_i + loss_t) / 2
+
+        return loss
+    
+    def top_image(self, images, text):
+        text = self.tokenizer(text).to(self.device)
+        caption_embed = self.caption_encoder(text["input_ids"], text["attention_mask"])
+
+        similarities = []
+
+        for image in images:
+            image_embed = self.vision_encoder(image.to(self.device))
+            similarities.append(F.cosine_similarity(image_embed, caption_embed, dim=1).item())
+
+        #top_image = np.argsort(similarities)[-1:][::-1]
+
+        return similarities
+    
+def metrics(similarity: torch.Tensor):
+    y = torch.arange(len(similarity)).to(similarity.device)
+    img2cap_match_idx = similarity.argmax(dim=1)
+    cap2img_match_idx = similarity.argmax(dim=0)
+
+    img_acc = (img2cap_match_idx == y).float().mean()
+    cap_acc = (cap2img_match_idx == y).float().mean()
+
+    return img_acc, cap_acc
